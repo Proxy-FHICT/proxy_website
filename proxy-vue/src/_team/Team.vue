53c838e59e4d25846747000fb6ee59f6285bdad3
Dmitry <dmitry-orlov@hotmail.com>
2017-09-23 01:39:15 +0200

    team page imporovements only for desktop version

--- proxy-vue/src/_team/Team.vue
+++ proxy-vue/src/_team/Team.vue
@@ -29,46 +29,72 @@
                         Departments
                     </h1>
                 </div>
-                <div class="row top-margin">
-                    <div class="c4 columns centered-hor">
-                        <!-- pictures need to be changed -->
-                        <img src="../assets/lnr-thumbs-up.svg" style="height: 80px;" class="value-img" />
-                        <h5>Administration</h5>
-                        <h6>Meet other students, share drinks and have fun</h6>
+
+                <!-- Determine which component to load based on the screen size -->
+                <span v-for="d_row in departments"> <!-- ITERATOR -->
+             
+                    <div class="row centered-hor">
+                        <div class="c4 columns centered-hor">
+
+                            <!-- First of the first row -->
+                            <div class="top-margin">        
+                                <!-- pictures need to be changed -->
+                                <img src="../assets/lnr-thumbs-up.svg" style="height: 80px;" class="value-img" />
+                                <h5>{{d_row.instances[0].title}}</h5>
+                                <h6>{{d_row.instances[0].text}}</h6>
+                                <a @click="selectDepartment(d_row, 0)">{{MORE_ANCHOR}}</a>
+                            </div>
+                            
+                            <!-- The shifted ones -->
+                            <span v-if="!isSelectorEmpty(d_row)">
+                                <div class="top-margin">        
+                                    <img src="../assets/lnr-thumbs-up.svg" style="height: 80px;" class="value-img" />
+                                    <h5>{{d_row.instances[1].title}}</h5>
+                                    <h6>{{d_row.instances[1].text}}</h6>
+                                    <a @click="selectDepartment(d_row, 1)">{{MORE_ANCHOR}}</a>
+                                </div>
+                                
+                                <div class="top-margin">        
+                                    <img src="../assets/lnr-thumbs-up.svg" style="height: 80px;" class="value-img" />
+                                    <h5>{{d_row.instances[2].title}}</h5>
+                                    <h6>{{d_row.instances[2].text}}</h6>
+                                    <a @click="selectDepartment(d_row, 2)">{{MORE_ANCHOR}}</a>
+                                </div>
+                            </span>
+                        </div>
+
+                        <!-- The selected one -->
+                        <div id="departmentDetailsPane"
+                                class="c8 columns centered-hor top-margin"  
+                                v-if="!isSelectorEmpty(d_row)">
+                            <h1><i class="fa fa-angle-left fa-1" 
+                                    @click="clearSelection()"></i> 
+                                    {{d_row.selected.title}}</h1>
+                        </div>
+
+                        <!-- The normal rest (none selected) -->
+                        <div class="c4 columns centered-hor" v-if="isSelectorEmpty(d_row)">
+                            <div class="top-margin">      
+                                <!-- pictures need to be changed -->
+                                <img src="../assets/lnr-thumbs-up.svg" style="height: 80px;" class="value-img" />
+                                <h5>{{d_row.instances[1].title}}</h5>
+                                <h6>{{d_row.instances[1].text}}</h6>
+                                <a @click="selectDepartment(d_row, 1)">{{MORE_ANCHOR}}</a>
+                            </div>
+                        </div>
+                        <div class="c4 columns centered-hor" v-if="isSelectorEmpty(d_row)">
+                            <div class="top-margin">      
+                                <!-- pictures need to be changed -->
+                                <img src="../assets/lnr-thumbs-up.svg" style="height: 80px;" class="value-img" />
+                                <h5>{{d_row.instances[2].title}}</h5>
+                                <h6>{{d_row.instances[2].text}}</h6>
+                                <a @click="selectDepartment(d_row, 2)">{{MORE_ANCHOR}}</a>
+                            </div>
+                        </div>
+                        
                     </div>
-                    <div class="c4 columns centered-hor">
-                        <!-- pictures need to be changed -->
-                        <img src="../assets/lnr-thumbs-up.svg" style="height: 80px;" class="value-img" />
-                        <h5>Assisting</h5>
-                        <h6>Meet other students, share drinks and have fun</h6>
-                    </div>
-                    <div class="c4 columns centered-hor">
-                        <!-- pictures need to be changed -->
-                        <img src="../assets/lnr-thumbs-up.svg" style="height: 80px;" class="value-img" />
-                        <h5>Entertainment & Social</h5>
-                        <h6>Meet other students, share drinks and have fun</h6>
-                    </div>
-                </div>
-                <div class="row top-margin">
-                    <div class="c4 columns centered-hor">
-                        <!-- pictures need to be changed -->
-                        <img src="../assets/lnr-thumbs-up.svg" style="height: 80px;" class="value-img" />
-                        <h5>Infrastructure</h5>
-                        <h6>Meet other students, share drinks and have fun</h6>
-                    </div>
-                    <div class="c4 columns centered-hor">
-                        <!-- pictures need to be changed -->
-                        <img src="../assets/lnr-thumbs-up.svg" style="height: 80px;" class="value-img" />
-                        <h5>Internal & External</h5>
-                        <h6>Meet other students, share drinks and have fun</h6>
-                    </div>
-                    <div class="c4 columns centered-hor">
-                        <!-- pictures need to be changed -->
-                        <img src="../assets/lnr-thumbs-up.svg" style="height: 80px;" class="value-img" />
-                        <h5>Teaching</h5>
-                        <h6>Meet other students, share drinks and have fun</h6>
-                    </div>
-                </div>
+
+                </span> <!-- END OF ITERATOR -->
             </div>
         </section>
 
@@ -78,17 +104,78 @@
 </template>
 
 <script>
+
+    // TOOD: add dynamic loading of the departments
+    // TODO: change department pictures
+    // TODO: add animations 
+
     import Nav from '../_shared/Navigation.vue'
     import Footer from '../_shared/Footer.vue'
 
     export default {
         data() {
           return {
-              title: "TEAM",
-              anchors: [
+            title: "TEAM",
+            MORE_ANCHOR: "more...",
+            anchors: [
+                
+            ],
+            departments: [
+                {
                    
-              ]
+                    instances:[
+                        {
+                            title: 'Administration',
+                            text:'Feed other students, share drinks and have fun'
+                        },
+                        {
+                            title: 'Internal & External',
+                            text:'Meet other students, share drinks and have fun'
+                        },
+                        {
+                            title: 'Internal & External',
+                            text:'Meet other students, share drinks and have fun'
+                        }
+                    ],
+                    selected: {},
+                },
+                {
+                    instances:[
+                        {
+                            title: 'Infrastructure',
+                            text:'Meet other students, share drinks and have fun'
+                        },
+                        {
+                            title: 'Internal & External',
+                            text:'Meet other students, share drinks and have fun'
+                        },
+                        {
+                            title: 'Teaching',
+                            text:'Meet other students, share drinks and have fun'
+                        }
+                    ],
+                    selected: {},
+                }
+            ],
           }
+        },
+        // computed: {
+        //     isSelectorActive: function(d_row) {
+        //         return d_row.selected;
+        //     }
+        // },
+        methods: {
+            selectDepartment: function(row, index) {
+                console.log('selecting');
+                for(let i in this.departments){this.departments[i].selected = {};}
+                row.selected = row.instances[index];
+            },
+            clearSelection: function() {
+                for(let i in this.departments){this.departments[i].selected = {};}
+            },
+            isSelectorEmpty: function(row){
+                return Object.keys(row.selected).length === 0 && row.selected.constructor === Object
+            }
         },
         created(){
 
